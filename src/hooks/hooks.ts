473d10d42c7cbf8733b7f22025b7dc2ed2f33b73
hem <hem@quicktext.im>
2025-12-09 17:25:23 +0100

    Add TypeScript configuration and enhance Cucumber hooks for Playwright integration

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,17 +1,37 @@
-import { AfterAll, BeforeAll } from "@cucumber/cucumber";
+import { After, AfterAll, Before, BeforeAll, Status } from "@cucumber/cucumber";
 
-import { chromium, Browser, Page } from "playwright";
+import { chromium, Browser, BrowserContext } from "playwright";
 import { pageFixture } from "./pageFixture";
 let browser: Browser;
-let page: Page;
+let context: BrowserContext;
 
 BeforeAll(async function () {
   browser = await chromium.launch({ headless: false });
-  page = await browser.newPage();
+});
+
+Before(async function () {
+  context = await browser.newContext();
+  const page = await browser.newPage();
   pageFixture.page = page;
 });
 
-AfterAll(async function () {
+After(async function ({ pickle, result }) {
+  if (result?.status === Status.FAILED) {
+    const img = await pageFixture.page.screenshot({
+      path: `./test-results/screenshots/failed/${pickle.name}`,
+      type: "png",
+    });
+    await this.attach(img, "image/png");
+  }
+  const img = await pageFixture.page.screenshot({
+    path: `./test-results/screenshots/success/${pickle.name}`,
+    type: "png",
+  });
+  await this.attach(img, "image/png");
   await pageFixture.page.close();
+  await context.close();
+});
+
+AfterAll(async function () {
   await browser.close();
 });
